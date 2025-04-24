b67a9634990b652a6d4d53010feda91c071f2d86
najuu24 <anajulia.vieira.tds24@gmail.com>
2025-04-24 17:26:28 -0300

    turmaDTO ok

--- Escola/Escola/src/main/java/com/example/Escola/DTO/TurmaDTO.java
+++ Escola/Escola/src/main/java/com/example/Escola/DTO/TurmaDTO.java
@@ -1,4 +1,36 @@
 package com.example.Escola.DTO;
 
-public class TurmaDTO {
+import com.example.Escola.Entity.Turma;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TurmaDTO implements Serializable {
+    private Long id;
+    private String sigla;
+    private int numeroSala;
+    private String nome;
+
+    public Turma toTurma() {
+        return new Turma(
+                this.id,
+                this.sigla,
+                this.numeroSala,
+                this.nome
+        );
+    }
+
+    public TurmaDTO fromTurma(Turma turma) {
+        return new TurmaDTO(
+                turma.getId(),
+                turma.getSigla(),
+                turma.getNumeroSala(),
+                turma.getNome()
+        );
+    }
 }
--- Escola/Escola/src/main/java/com/example/Escola/Entity/Turma.java
+++ Escola/Escola/src/main/java/com/example/Escola/Entity/Turma.java
@@ -1,4 +1,22 @@
 package com.example.Escola.Entity;
 
-public class Turma {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Turma implements Serializable {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String sigla;
+    private int numeroSala;
+    private String nome;
 }
