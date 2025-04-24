86b9bee409092971845be8f560e9e9bdd3eb5d6a
HeyKi24 <rzz.chiara24@gmail.com>
2025-04-24 16:08:22 -0300

    FRango frito

--- Escola/Escola/src/main/java/com/example/Escola/DTO/ProfessorDTO.java
+++ Escola/Escola/src/main/java/com/example/Escola/DTO/ProfessorDTO.java
@@ -1,4 +1,33 @@
 package com.example.Escola.DTO;
 
-public class ProfessorDTO {
+import com.example.Escola.Entity.Professor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfessorDTO implements Serializable {
+    private Long id;
+    private String nome;
+    private String sobrenome;
+
+    public Professor toProfessor(){
+        return new Professor(
+                this.id,
+                this.nome,
+                this.sobrenome
+        );
+    }
+
+    public ProfessorDTO fromProfessor(Professor professor){
+        return new ProfessorDTO(
+            professor.getId(),
+            professor.getNome(),
+            professor.getSobrenome()
+        );
+    }
 }
